a30341f0567091078b75f998f59d87f2ab0e2a7b
ibrahimrahhal <ibrhaim.rahhal3636@gmail.com>
2018-06-02 18:30:23 +0300

    sm mobile styling

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 var menuico = document.getElementsByClassName("menu-ico")[0];
 var menunav = document.getElementById("nav-menu1");
 var br = document.getElementById("hero-con");
+var menupar = document.getElementsByClassName("menu-perent")[0];
 menuico.addEventListener("click",function(){
   menunav.classList.toggle("no-sm");
     br.classList.toggle("no-sm") ;
-    
+    menupar.classList.toggle("no-sm") ;
     
 })
