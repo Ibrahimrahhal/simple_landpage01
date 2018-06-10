f8dc1f4d7bc0b95c00573ad18cd135d645abf63a
Ibrahim Rahhal <ibrahim.rahhal3636@gmail.com>
2018-06-10 05:52:33 +0300

    some transitions and css arrangments

--- main.js
+++ main.js
@@ -2,10 +2,25 @@ var menuico = document.getElementsByClassName("menu-ico")[0];
 var menunav = document.getElementById("nav-menu1");
 var br = document.getElementById("hero-con");
 var menupar = document.getElementsByClassName("menu-perent")[0];
+var appleico = document.getElementsByClassName("apple")[0];
+var googleico = document.getElementsByClassName("google")[0];
 menuico.addEventListener("click",function(){
   menunav.classList.toggle("no-sm");
     br.classList.toggle("no-sm") ;
     menupar.classList.toggle("no-sm") ;
     menuico.classList.toggle("menu-ico-clicked");
     
+})
+
+appleico.addEventListener("mouseover",function(){
+    appleico.innerHTML= '<span class="btn-text">app store</span>' ;
+})
+appleico.addEventListener("mouseout",function(){
+    appleico.innerHTML= '<span id="apple-icon"></span>' ;
+})
+googleico.addEventListener("mouseover",function(){
+   googleico.innerHTML= "<span class='btn-text'>play store</span>" ;
+})
+googleico.addEventListener("mouseout",function(){
+    googleico.innerHTML= '<span id="google-icon"></span>' ;
 })
